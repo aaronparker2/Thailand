98315a339ca29de651423437392fc133f169ea1f
Aaron Parker <aaronp327@gmail.com>
2016-10-26 22:01:45 -0600

    Add project files.

---
+++ Thailand/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Thailand.Startup))]
+namespace Thailand
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
